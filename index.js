d806e88c2704d2d5ce43918964d0152ad261a062
BYAR PAR <byarpar0@gmail.com>
2024-10-23 16:01:05 +0630

    first commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = 3001;
 // Appwrite SDK Setup
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1') // Appwrite endpoint
-    .setProject('67150ead0038b4908893')          // Appwrite Project ID
+    .setProject('67150ead0038b4908893')          // Correct Project ID
     .setKey('standard_2331d9a8679e7ad53aea16d73ecee6a53c9956bdbade2260f8085cd91555aaa894970d216916e51675e5278b5d27de401a78824d45985bc623beb3e3db51dafc37a0532708f3481a6cc3e17ebcd1f0dcbcd0d96d5c0668d5ea25be8300955d9c695b264d1ad7020569df25a721c9593447a3f85b7629e718703c859a92a644f6'); // API key
 
 const database = new Databases(client);
@@ -23,8 +23,6 @@ const collectionId = '671730ea002925e55ce6'; // Collection ID
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// Routes for Database Operations
 
 // Fetch all contacts
 app.get('/contact', async (req, res) => {
@@ -44,7 +42,7 @@ app.post('/contact', async (req, res) => {
         const response = await database.createDocument(
             databaseId,
             collectionId,
-            'unique()', // Generate a unique ID
+            'unique()',  // Generate a unique ID
             { firstName, lastName, email, company, phone }
         );
         res.status(201).json(response);
@@ -54,7 +52,7 @@ app.post('/contact', async (req, res) => {
     }
 });
 
-// Route to list all function executions
+// List all function executions
 app.get('/functions/:functionId/executions', async (req, res) => {
     const { functionId } = req.params;
     try {
@@ -66,7 +64,7 @@ app.get('/functions/:functionId/executions', async (req, res) => {
     }
 });
 
-// Route to trigger a function using axios
+// Trigger a function using axios
 app.post('/trigger-function/:functionId', async (req, res) => {
     const { functionId } = req.params;
     try {
@@ -90,27 +88,3 @@ app.post('/trigger-function/:functionId', async (req, res) => {
 
 // Server setup
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
-// Function to trigger Appwrite function manually
-async function triggerAppwriteFunction(functionId) {
-    try {
-        const response = await axios.post(
-            `https://cloud.appwrite.io/v1/functions/${functionId}/executions`,
-            {}, // Empty body
-            {
-                headers: {
-                    'x-appwrite-project': '67150ead0038b4908893', // Project ID
-                    'x-appwrite-key': 'standard_2331d9a8679e7ad53aea16d73ecee6a53c9956bdbade2260f8085cd91555aaa894970d216916e51675e5278b5d27de401a78824d45985bc623beb3e3db51dafc37a0532708f3481a6cc3e17ebcd1f0dcbcd0d96d5c0668d5ea25be8300955d9c695b264d1ad7020569df25a721c9593447a3f85b7629e718703c859a92a644f6', // API Key
-                    'Content-Type': 'application/json'
-                }
-            }
-        );
-        console.log('Function executed:', response.data);
-    } catch (error) {
-        console.error('Error executing function:', error.response ? error.response.data : error.message);
-    }
-}
-
-// Example manual trigger call
-// Replace 'YOUR_FUNCTION_ID' with actual function ID
-triggerAppwriteFunction('YOUR_FUNCTION_ID');
